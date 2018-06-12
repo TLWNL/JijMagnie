191572f4e6f5387bf445acd94e66b14637ffc83e
TLWNL <dylanrijnsburger@gmail.com>
2018-06-12 20:05:42 +0200

    Switched from Buffer to Struct, fixed all bugs. Completed test 1.

--- src/machine.c
+++ src/machine.c
@@ -3,15 +3,21 @@
 #include <stdio.h>
 #include <assert.h>
 
-uint32_t *buffer;
-uint32_t byte1;				// Check if you can make these local variables, STRUCT?! That may look kewl
-uint32_t byte2;
-uint32_t byte3;
-uint32_t byte4;
 int program_counter = 0;
-uint32_t current_byte;
-long filelength;
-int amount_of_words;
+byte_t current_byte;
+bool byte_argument;
+bool short_argument;
+
+struct CURRENT{
+	word_t magic_number;
+	word_t constant_pool_origin;
+	word_t constant_pool_size;
+	word_t *constant_data;
+	word_t text_origin;
+	word_t text_size;
+	byte_t *text_data;
+};
+struct CURRENT DATA;
 
 
 static uint32_t swap_uint32(uint32_t num)
@@ -24,328 +30,283 @@ int init_ijvm(char *binary_file)
 	FILE *fp;			
 
 	fp = fopen(binary_file, "rb");				// Opens the file in binary mode 
-	// Get the file size
-	fseek(fp, 0 , SEEK_END);					// Jump to the end of the file
-	filelength = ftell(fp);						// Get the current byte offset
-	amount_of_words = filelength/4;
-	//printf("The total size of the binary file = %ld bytes\n", filelength);
-	rewind(fp);									// Jump back to the beginning of the file 
 
-	// Allocate memory for the entire file 
-	buffer = (uint32_t*) malloc(filelength+1);	// Allocate memory for the file + \0 
-	//Add error checking here
-
-	// Copy a word into the buffer 
-	fread(buffer,sizeof(uint32_t), filelength, fp);
-	fclose(fp);
-
-	// Change endian
-	for (int i=0; i<amount_of_words+1; i++)
+	// Copy magic number into the struct 
+	fread(&DATA.magic_number,sizeof(word_t), 1, fp);
+	//Change endian
+	DATA.magic_number = swap_uint32(DATA.magic_number);
+		// Error checking for the case that the magic number is not 1deadfad
+	if (DATA.magic_number != 0x1deadfad)
 	{
-		buffer[i] = swap_uint32(buffer[i]);
-	}
-	// Error checking for the case that the magic number is not 1deadfad
-	if (buffer[0] != 0x1deadfad)
-	{
-		printf("%02x", buffer[0]);
-		fprintf(stderr, "Binary file not supported, only .ijvm files supported. Program aborted.\n");
+		fprintf(stderr, "Binary file not supported, only .ijvm files are supported. Program aborted.\n");
 		return -1;
 	}
+
+	// Copy constant pool origin into the struct
+	fread(&DATA.constant_pool_origin,sizeof(word_t), 1, fp);
+	DATA.constant_pool_origin = swap_uint32(DATA.constant_pool_origin);
+
+	// Copy constant pool size into the struct
+	fread(&DATA.constant_pool_size, sizeof(word_t), 1, fp);
+	DATA.constant_pool_size = swap_uint32(DATA.constant_pool_size);
+
+	// Allocate memory for the constant pool data
+	DATA.constant_data = malloc(DATA.constant_pool_size);
+	// Read constant pool into the mallocated space
+	fread(DATA.constant_data, sizeof(word_t), DATA.constant_pool_size / 4, fp);	// Constant pool size is in bytes, constant data is in words, so divide by 4
+	// Change endian of the constant pool data
+	for(unsigned int i=0; i<DATA.constant_pool_size / 4; i++)
+	{
+		DATA.constant_data[i] = swap_uint32(DATA.constant_data[i]);
+	}
+
+	// Read text origin into the struct
+	fread(&DATA.text_origin, sizeof(word_t), 1, fp);
+	DATA.text_origin = swap_uint32(DATA.text_origin);
+
+	// Read text size into the struct
+	fread(&DATA.text_size, sizeof(word_t), 1, fp);
+	DATA.text_size = swap_uint32(DATA.text_size);
+
+	// Allocate memory for the text data
+	DATA.text_data = malloc(DATA.text_size);
+	// Read the text data in bytes
+	fread(DATA.text_data, sizeof(byte_t), DATA.text_size, fp);
 
 	return 0;	
 }
 
 void destroy_ijvm()
 {
-  free(buffer);
-  exit(0);
+	free(DATA.text_data);
+	free(DATA.constant_data);
  }
 
 void run()
 {
-	int constant_size;
-	int j;
 
-	// Run through the buffer word for word
-	for(int i = 1; i < amount_of_words+1; i++)		// Do I read through an extra word here?
-	{		
-		byte1 = (buffer[i]>>24) & 0xff;
-		byte2 = (buffer[i]>> 16) & 0xff;
-		byte3 = (buffer[i]>> 8) & 0xff;
-		byte4 = (buffer[i] & 0xff);
-		
-		if(i == 1)
+	byte_t stored_byte_argument;
+	byte_t stored_short_argument[2];
+	int arguments_passed = 0;
+
+	for (unsigned int i=0; i<DATA.text_size; i++)
+	{
+		current_byte = DATA.text_data[i];
+
+		if(byte_argument == true)
 		{
-			//printf("The origin of the constant pool is %02x\n", buffer[i]);
+			stored_byte_argument = current_byte;
+			byte_argument = false;
 			continue;
 		}
-		// Start reading the constants
-		else if(i == 2)
+		if(short_argument == true)
 		{
-			constant_size = buffer[i];
-			//printf("The size of the constant pool in bytes is: %d \n", constant_size);
-			// Allocate memory for the constant array?
-			uint32_t constants[constant_size];
-			for (j = 2; j < constant_size+2; j++)
+			if (arguments_passed == 0)
 			{
-				constants[j] = buffer[i];
+				stored_short_argument[0] = current_byte;
+				arguments_passed += 1;
+				continue;
 			}
-			i += constant_size/4;
+			if (arguments_passed == 1)
+			{
+				stored_short_argument[1] = current_byte;
+				arguments_passed = 0;
+				short_argument = false;
+				continue;
+			}
 		}
-		else if(i == constant_size/4 + 3)			// This is the word directly after the constant pool: the origin of TEXT
-		{
-			//printf("The origin of the TEXT pool is: %02x \n", buffer[i]);
-			continue;
-		}
-		else if(i == constant_size/4 + 4)			// This is the size of the text pool.
-		{
-			//printf("The size of the TEXT pool is: %d \n", buffer[i]);
-			continue;
-		}
-		else
-		{
-			step();
-		}
-	}	
+		step();
+	}
 }
 
 bool step()
 {
-	bool byte_argument = false;
-	bool short_argument;
-
-	for(int i=0; i<4; i++)
+	switch(current_byte)
 	{
-
-		switch(i)
+		case 0x10: // Takes byte arg
 		{
-			case 0:
-			{
-				current_byte = byte1;
-				//printf("Current byte = %02x \n", current_byte);
-				break;
-			}
-			case 1:
-			{
-				current_byte = byte2;
-				//printf("Current byte = %02x \n", current_byte);
-				break;
-			}
-			case 2:
-			{
-				current_byte = byte3;
-				//printf("Current byte = %02x \n", current_byte);
-				break;
-			}
-			case 3:
-			{
-				current_byte = byte4;
-				//printf("Current byte = %02x \n", current_byte);
-				break;
-			}
+			printf("BIPUSH \n");
+			program_counter = program_counter + 1;
+			byte_argument = true;
+			break;
 		}
-
-		if(byte_argument == true)
+		case 0x59:
 		{
-			//printf("Byte argument = %02x\n", current_byte);
-			byte_argument = false;
-			continue;
+			printf("DUP\n");
+			program_counter = program_counter + 1;
+			break;
 		}
-		
-		switch(current_byte)		// Check if increasing the i really works 
+		case 0xfe:
 		{
-			case 0x10: // Takes byte arg
-			{
-				printf("BIPUSH \n");
-				program_counter = program_counter + 1;
-				byte_argument = true;
-				break;
-			}
-			case 0x59:
-			{
-				printf("DUP\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0xfe:
-			{
-				printf("ERR\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0xa7:
-			{
-				printf("GOTO\n");			// Takes short arg ( 2 bytes )
-				program_counter = program_counter + 1;
-				short_argument = true;
-				i += 2;
-				break;
-			}
-			case 0xff:
-			{
-				destroy_ijvm();
-				printf("HALT\n");
-				//program_counter = program_counter + 1;
-				//break;
-			}
-			case 0x60:
-			{
-				printf("IADD\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x7e:
-			{
-				printf("IAND\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x99:
-			{
-				printf("IFEQ\n");			// Takes short arg 
-				program_counter = program_counter + 1;
-				short_argument = true;
-				break;
-			}
-			case 0x9b:
-			{
-				printf("IFLT\n");			// Takes short arg 
-				program_counter = program_counter + 1;
-				short_argument = true;
-				break;
-			}
-			case 0x9f:
-			{
-				printf("IF_ICMPEQ\n");	// Takes short arg 
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x84:
-			{
-				printf("IINC\n");			// Takes byte byte arg 
-				program_counter = program_counter + 1;
-				short_argument = true;
-				break;
-			}
-			case 0x15:
-			{
-				printf("ILOAD\n");		// Takes byte arg
-				program_counter = program_counter + 1;
-				byte_argument = true;
-				break;
-			}
-			case 0xfc:
-			{
-				printf("IN\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0xb6:
-			{
-				printf("INVOKEVIRTUAL\n");	// Takes short arg
-				program_counter = program_counter + 1;
-				short_argument = true;
-				break;
-			}
-			case 0xb0:
-			{
-				printf("IOR\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0xac:
-			{
-				printf("IRETURN\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x36:
-			{
-				printf("ISTORE\n");			// Takes byte arg
-				program_counter = program_counter + 1;
-				byte_argument = true;
-				break;
-			}
-			case 0x64:
-			{
-				printf("ISUB\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x13:
-			{
-				printf("LDC_W\n");			// Takes short arg
-				program_counter = program_counter + 1;
-				short_argument = true;
-				break;
-			}
-			case 0x00:
-			{
-				printf("NOP\n");
-				//program_counter = program_counter + 1;			// Does the program counter get increased after 00?
-				break;
-			}
-			case 0xfd:
-			{
-				printf("OUT\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x57:
-			{
-				printf("POP\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0x5f:
-			{
-				printf("SWAP\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			case 0xc4:
-			{
-				printf("WIDE\n");
-				program_counter = program_counter + 1;
-				break;
-			}
-			// Add EOF case
+			printf("ERR\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0xa7:
+		{
+			printf("GOTO\n");			// Takes short arg ( 2 bytes )
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0xff:
+		{
+			printf("HALT\n");
+			//program_counter = program_counter + 1;
+			//break;
+		}
+		case 0x60:
+		{
+			printf("IADD\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0x7e:
+		{
+			printf("IAND\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0x99:
+		{
+			printf("IFEQ\n");			// Takes short arg 
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0x9b:
+		{
+			printf("IFLT\n");			// Takes short arg 
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0x9f:
+		{
+			printf("IF_ICMPEQ\n");	// Takes short arg 
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0x84:
+		{
+			printf("IINC\n");			// Takes byte byte arg 
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0x15:
+		{
+			printf("ILOAD\n");		// Takes byte arg
+			program_counter = program_counter + 1;
+			byte_argument = true;
+			break;
+		}
+		case 0xfc:
+		{
+			printf("IN\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0xb6:
+		{
+			printf("INVOKEVIRTUAL\n");	// Takes short arg
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0xb0:
+		{
+			printf("IOR\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0xac:
+		{
+			printf("IRETURN\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0x36:
+		{
+			printf("ISTORE\n");			// Takes byte arg
+			program_counter = program_counter + 1;
+			byte_argument = true;
+			break;
+		}
+		case 0x64:
+		{
+			printf("ISUB\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0x13:
+		{
+			printf("LDC_W\n");			// Takes short arg
+			program_counter = program_counter + 1;
+			short_argument = true;
+			break;
+		}
+		case 0x00:
+		{
+			printf("NOP\n");
+			program_counter = program_counter + 1;			// Does the program counter get increased after 00?
+			break;
+		}
+		case 0xfd:
+		{
+			printf("OUT\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0x57:
+		{
+			printf("POP\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0x5f:
+		{
+			printf("SWAP\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		case 0xc4:
+		{
+			printf("WIDE\n");
+			program_counter = program_counter + 1;
+			break;
+		}
+		// Add EOF case
 
-			// Add \n case
+		// Add \n case
 
-			// Change default to somtething that makes sense
-			default:
-			{
-				printf("This is the default message\n");
-				break;
-			}
+		// Change default to somtething that makes sense
+		default:
+		{
+			printf("This is the default message\n");
+			break;
 		}
 	}
+
 	//get_program_counter();
 	return 0;
 }
 
 byte_t *get_text()
 {
-
-	// Print the buffered data 
-	for (int i = 0; i < amount_of_words+1; i++)
-	{
-		printf("Current buffer item: %02x \n", buffer[i]);
-	}
-	return 0;
+	return DATA.text_data;
 }
 
 int get_program_counter()
 {
-	printf("The current program counter is: %d \n", program_counter);
+	//printf("The current program counter is: %d \n", program_counter);
 	return program_counter;
 }
 
 byte_t get_instruction()
 {
-	printf("The current instruction is: %02x \n", current_byte);
-
+	//printf("The current instruction is: %02x \n", current_byte);
 	return current_byte;
 }
 
@@ -357,4 +318,10 @@ void set_input(FILE *fp)
 void set_output(FILE *fp)
 {
   // TODO: implement me
+}
+
+int text_size()
+{
+	int text_size_int = DATA.text_size;
+	return text_size_int;
 }
